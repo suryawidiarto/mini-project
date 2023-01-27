5e35059c0dbcde8cf3e481979fe5dca71ae0f7e6
Surya <s.adiwidiarto@gmail.com>
2023-01-27 16:11:59 +0700

    [mini-project] add service-api-gateway

---
+++ service-api-gateway/src/main/java/com/sdevcode/project/serviceapigateway/ServiceApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.sdevcode.project.serviceapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApiGatewayApplication.class, args);
+	}
+
+}
